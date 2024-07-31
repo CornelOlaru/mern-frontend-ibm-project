b0aade803dcc963bf6ba14adacbc3152378d6996
Corneliu Olaru <cornel.olaru47@gmail.com>
2024-07-31 11:16:23 +0300

    designed signup, login forms and footer

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,62 +1,98 @@
-import React, { useState } from 'react'
-import "./login.css"
-import { Link, useNavigate } from 'react-router-dom'
-import Navbar from '../../components/navbar/Navbar';
+import React, { useState } from "react";
+import "./login.css";
+import { Link, useNavigate } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
 const Login = () => {
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3001/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const result = await response.json();
+      localStorage.setItem("token", result.token);
+      console.log(result);
+      if (response.ok) {
+        navigate("/");
+        return result;
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setFormData({
+        name: "",
         email: "",
-        password: ""
-    })
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({
-          ...formData,
-          [name]: value,
-        });
-      };
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-        const response = await fetch("https://mern-backend-ibm-project.vercel.app/auth/login", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              
-            },
-            body: JSON.stringify(formData),
-          });
-          const result = await response.json();
-          localStorage.setItem("token", result.token)
-          console.log(result);
-          if(response.ok) {
-              navigate("/");
-            return result
-          } 
-        } catch (error) {
-          console.log(error.message);
-        } finally {
-          setFormData({
-            name: "",
-            email: "",
-            password: "",
-          });
-        }
-      };
+        password: "",
+      });
+    }
+  };
   return (
     <main>
-        <Navbar/>
-      <form className='login-form' onSubmit={handleSubmit}>
-        <h1 className='form-title'>Login</h1>
-        <label htmlFor="email">Email</label>
-        <input id='email' name='email' type="email" placeholder='Email' value={formData.email} onChange={handleInputChange}/>
-        <label htmlFor="password">Password</label>
-        <input id='password' name='password' type="password" placeholder='Password' value={formData.password} onChange={handleInputChange}/>
-        <button className='login-button' type='submit'>Login</button>
-        <Link to="/register">Don't have an account?</Link>
-      </form>
-    </main>
-  )
-}
+      <Navbar />
+      <div className="login-container">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <h1 className="form-title">Login</h1>
 
-export default Login
+          <div className="form-row">
+            <label className="form-label">
+              Email
+              </label>
+            <input
+              className="form-input"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+              />
+              </div>
+
+          <div className="form-row">
+            <label className="form-label" >
+              Password
+            </label>
+
+            <input
+              className="form-input"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <button className="form-button" type="submit">
+            Login
+          </button>
+          <Link className="register-link" to="/register">
+            Don't have an account?
+          </Link>
+        </form>
+      </div>
+      <Footer/>
+    </main>
+  );
+};
+
+export default Login;
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./signup.css";
 import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -21,7 +22,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-    const response = await fetch("https://mern-backend-ibm-project.vercel.app/user/register", {
+      const response = await fetch("http://localhost:3001/user/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -30,10 +31,10 @@ const Signup = () => {
       });
       const result = await response.json();
       console.log(result);
-      if(response.ok) {
-          navigate("/login");
-        return result
-      } 
+      if (response.ok) {
+        navigate("/login");
+        return result;
+      }
     } catch (error) {
       console.log(error.message);
     } finally {
@@ -44,44 +45,61 @@ const Signup = () => {
       });
     }
   };
- 
+
   return (
     <main>
-        <Navbar/>
+      <Navbar />
+        <div className="signup-container">
+
       <form onSubmit={handleSubmit} className="signup-form">
-        <h1 className="signup-title">Sign up</h1>
-        <label htmlFor="name">Name</label>
-        <input
-          name="name"
-          id="name"
-          type="text"
-          placeholder="Name"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          id="email"
-          type="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleInputChange}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          id="password"
-          type="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleInputChange}
-        />
+        <h1 className="form-title">Sign up</h1>
+        <div className="form-row">
+          <label className="form-label">Name</label>
+          <input
+          className="form-input"
+            name="name"
+            id="name"
+            type="text"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleInputChange}
+            />
+        </div>
+        <div className="form-row">
+          <label className="form-label">Email</label>
+          <input
+            className="form-input"
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Enter email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+            />
+        </div>
+
+        <div className="form-row">
+          <label className="form-label">Password</label>
+
+          <input
+            className="form-input"
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+            />
+        </div>
         <button className="form-button" type="submit">
           Sign up
         </button>
-        <Link to="/login">Already have an account?</Link>
+        <Link className="login-link" to="/login">Already have an account?</Link>
       </form>
+            </div>
+            <Footer/>
     </main>
   );
 };
