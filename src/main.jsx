5395d600a380b68355763aa56ad3cfab5b9aa189
Pasc Bogdan <matei.pasc03@e-uvt.ro>
2024-08-01 17:18:10 +0300

    about + contact

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import ProductDetails from "./pages/productPage/productDetails";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact"
 
 const router = createBrowserRouter([
   {
@@ -24,7 +26,14 @@ const router = createBrowserRouter([
     path: "product/:productId", 
     element: <ProductDetails />,
   },
- 
+  {
+    path: "about",
+    element: <About/>,
+  },
+  {
+    path: "contact",
+    element: <Contact/>,
+  }
 ]);
 
 createRoot(document.getElementById("root")).render(
